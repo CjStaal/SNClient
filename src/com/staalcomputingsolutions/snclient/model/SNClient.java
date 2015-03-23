fbd84f86586824aeccaed02c05733969931d9955
Charles Staal <Charles.Staal@gmail.com>
2015-03-22 22:49:47 -0400

    Created.

---
+++ src/com/staalcomputingsolutions/snclient/model/SNClient.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright [2015] [Charles Joseph Staal]
+ */
+package com.staalcomputingsolutions.snclient.model;
+
+/**
+ *
+ * @author Charles Joseph Staal
+ */
+public class SNClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
